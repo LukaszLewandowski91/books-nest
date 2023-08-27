a6f85e7154f0649be2f722f0a3cc544b49256b79
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-27 17:15:10 +0200

    add methods and endpoints getAll, getById, delete and post for books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dtos/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.booksService.getAll();
+  }
+
+  @Get('/:id')
+  async getyId(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.booksService.getById(id))) {
+      throw new NotFoundException('Book not found');
+    }
+    return this.booksService.getById(id);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.booksService.getById(id))) {
+      throw new NotFoundException('Book not found');
+    }
+    return this.booksService.delete(id);
+  }
+
+  @Post('/')
+  create(@Body() bookData: CreateBookDTO) {
+    return this.booksService.create(bookData);
+  }
+}
