d824116e72a2f84cd70f8aa7571bed9abb271dd5
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-28 19:52:02 +0200

    add local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,13 +4,25 @@ import { RegisterDTO } from './dtos/register.dto';
 import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
   public async register(registrationData: RegisterDTO) {
     const hashedPassword = await bcrypt.hash(registrationData.password, 10);
     const userData = {
       email: registrationData.email,
     };
-    return this.userService.create(userData, hashedPassword);
+    return this.usersService.create(userData, hashedPassword);
+  }
+
+  public async validateUser(email: string, password: string) {
+    const user = await this.usersService.getByEmail(email);
+    if (
+      user &&
+      (await bcrypt.compare(password, user.password.hashedPassword))
+    ) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
   }
 }
