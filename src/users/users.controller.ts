7e292d02ad9377d60fc4a20aaef68be96bbeb0f7
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-08-30 19:01:22 +0200

    add Admin guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import {
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
   ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -22,5 +26,16 @@ export class UsersController {
       throw new NotFoundException('User not found');
     }
     return this.userService.getById(id);
+  }
+
+  @Delete('/:id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.userService.getById(id))) {
+      throw new NotFoundException('User not found');
+    }
+    await this.userService.delete(id);
+    return { success: true };
   }
 }
